5df2dd224446a623e125da5dd8b1990dc5754998
anthony708 <anthony78@live.cn>
2019-05-29 17:30:29 +0800

    Two problems on Array and String each.

---
+++ src/Array/HeightChecker.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+
+/**
+ * 1051. Height Checker
+ * Easy
+ * @author zhuduan
+ * 2019/05/29
+ */
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int res = 0;
+        int [] newHeights = heights.clone();
+        Arrays.sort(newHeights);
+        for (int i = 0; i < heights.length; i++) {
+            if (newHeights[i] != heights[i]) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
