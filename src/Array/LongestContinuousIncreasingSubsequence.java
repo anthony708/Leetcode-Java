5625716303818c421dce9937b78f1638b5ce747f
anthony708 <anthony78@live.cn>
2019-07-16 17:43:14 +0800

    Two Array exercises on 16/7.

---
+++ src/Array/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,37 @@
+package Array;
+
+/**
+ * 674. Longest Continuous Increasing Subsequence
+ * Easy
+ * @author zhuduan
+ * 2019/07/16
+ */
+
+public class LongestContinuousIncreasingSubsequence {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int startIndex = 0;
+        int endIndex = 0;
+        int res = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                endIndex++;
+            } else {
+                res = Math.max(res, endIndex - startIndex + 1);
+                endIndex = i;
+                startIndex = i;
+            }
+        }
+
+        res = Math.max(res, endIndex - startIndex + 1);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LongestContinuousIncreasingSubsequence longestContinuousIncreasingSubsequence = new LongestContinuousIncreasingSubsequence();
+        int[] nums = {1,3,5,7};
+        System.out.println(longestContinuousIncreasingSubsequence.findLengthOfLCIS(nums));
+    }
+}
---
+++ src/Array/MaximumProductofThreeNumbers.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Arrays;
+
+/**
+ * 628. Maximum Product of Three Numbers
+ * Easy
+ * @author zhuduan
+ * 2019/07/16
+ */
+
+public class MaximumProductofThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        int res1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int res2 = nums[0] * nums[1] * nums[nums.length - 1];
+
+        return Math.max(res1, res2);
+    }
+}
