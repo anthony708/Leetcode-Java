a542942ed30b0aca72b3fc496824295efc56e9b5
Anthony708 <anthony78@live.cn>
2021-08-21 18:24:40 +0800

    upload 122

---
+++ src/com/github/anthony708/java/Leetcode/Greedy/MaxProfit.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: Anthony
+ * @Date: 2021-08-21 18:16:21
+ * @LastEditTime: 2021-08-21 18:17:15
+ */
+
+ package com.github.anthony708.java.LeetCode.Greedy;
+
+ class MaxProfit {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+ }
