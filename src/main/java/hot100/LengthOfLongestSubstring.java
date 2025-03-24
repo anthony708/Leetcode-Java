11ec61a044d2fd7e19a4aa31d526f38e279b5cd3
Duan Zhu <duan.zhu@shopee.com>
2025-03-24 19:02:43 +0800

    324

---
+++ src/main/java/hot100/LengthOfLongestSubstring.java
@@ -0,0 +1,61 @@
+package hot100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.transform.Source;
+
+/*
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+
+ 
+
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+
+class LengthOfLongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        //用一个 hashSet 存储不重复的字串的字符
+        // 用两个指针记录子串的左右边界l, r
+        // 每次移动右边界, 观察 hashSet 是否有不重复字符, 记录下这个 hashSet长度
+        int res = 0;
+        int r = - 1;
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0) {
+                charSet.remove(s.charAt(i - 1));
+            }
+
+            // 右边界向右移动
+            while (r + 1 < s.length() && !charSet.contains(s.charAt(r + 1))) {
+                charSet.add(s.charAt(r + 1));
+                r++;
+            }
+            res = Math.max(res, charSet.size());
+
+        }
+
+        return res;
+    }     
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        int res = lengthOfLongestSubstring(s);
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/hot100/Trap.java
@@ -0,0 +1,43 @@
+package hot100;
+
+/*
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+
+示例 1：
+
+输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+输出：6
+解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+示例 2：
+
+输入：height = [4,2,0,3,2,5]
+输出：9
+ */
+
+class Trap {
+    public int trap(int[] height) {
+        //维护双指针 l 和 r, 两个变量 lMax 和 rMax
+        // 下标 i 处能接到雨水的量 = Min(lMax[i], rMax[i]) 
+        // 双指针向中间移动, 用 height[l] 和 height[r] 更新 lMax 和 rMax
+        // 如果 height[l] < height[r], 则 lMax < rMax, 则能接到雨水为 lMax - height[i], l++
+        // 如果 height[l] >= height[r], 则 lMax >= rMax, 则能接到雨水为 rMax - height[i], r--
+        int res = 0;
+        int l = 0, r = height.length - 1;
+        int lMax = 0, rMax = 0;
+        while (l < r) {
+            lMax = Math.max(lMax, height[l]);
+            rMax = Math.max(rMax, height[r]);
+
+            if (height[l] < height[r]) {
+                res += lMax - height[l];
+                l++;
+            } else {
+                res += rMax - height[r];
+                r--;
+            }
+        }
+
+        return res;
+    }    
+}
